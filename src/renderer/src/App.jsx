2a39370fd542c16a595bcfecd28a7e13b781526b
MeBadDev <94725241+MeBadDev@users.noreply.github.com>
2023-09-24 08:56:38 +0800

    Give up using muuri

--- src/renderer/src/App.jsx
+++ src/renderer/src/App.jsx
@@ -1,25 +1,43 @@
-import "../../../node_modules/electron-tabs/dist/electron-tabs"
+import { DndProvider } from 'react-dnd'
+import { HTML5Backend } from 'react-dnd-html5-backend'
+
+
 export default function App() {
+
   return (
-    <>
-      <tab-group></tab-group>
-
-
-      <div className="w-full h-full absolute bg-ctp-base">
-        <Sidebar />
-      </div>
-    </>
+    <Editor />
   )
 }
 
 function Sidebar() {
+  const items = [
+    {"key": "folder"}, {"key": "find"}, {"key": "settings"}, {"key": "account"}, {"key": "extension_store"}
+  ]
+
+  const childrens = items.map((item) => {
+    return (<SidebarItem key={item.key} />)
+  })
   return (
     <div className="p-3 bg-ctp-crust w-min h-full flex flex-col">
-      <div className="w-12 aspect-square bg-ctp-blue m-1"></div>
-      <div className="w-12 aspect-square bg-ctp-blue m-1"></div>
-      <div className="w-12 aspect-square bg-ctp-blue m-1"></div>
-      <div className="w-12 aspect-square bg-ctp-blue m-1"></div>
-      <div className="w-12 aspect-square bg-ctp-blue m-1"></div>
+      <DndProvider backend={HTML5Backend}>
+        {childrens}
+      </DndProvider>
     </div>
+  )
+}
+
+function Editor() {
+  return (
+    <div className=" w-full h-full absolute">
+      <div className="w-full h-full absolute bg-ctp-base overflow-hidden min-h-0">
+        <Sidebar />
+      </div>
+    </div>
+  )
+}
+
+function SidebarItem() {
+  return (
+    <div className="w-12 aspect-square bg-ctp-blue m-1"></div>
   )
 }
